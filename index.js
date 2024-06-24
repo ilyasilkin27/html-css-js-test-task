71e55e5d55f1362f7abb10dbed360721c5071c95
ilyasilkin27 <ilyasilkin27@gmail.com>
2024-06-24 15:22:11 +0300

    add few features

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const trackYandexMetrikaGoal = (counterId, goal) => {
-    if (window[`yaCounter${counterId}`]) {
-        window[`yaCounter${counterId}`].reachGoal(goal);
+    const yandexCounter = window[`yaCounter${counterId}`];
+    if (yandexCounter) {
+        yandexCounter.reachGoal(goal);
     }
 };
 
@@ -10,6 +11,38 @@ const setupCallButtonListener = () => {
         if (callButton) {
             callButton.addEventListener('click', () => {
                 trackYandexMetrikaGoal('99999999', 'test-target');
+            });
+        }
+
+        const openPopupButton = document.querySelector('.cta_customer__button--primary');
+        const overlay = document.getElementById('overlay');
+        const closeButton = document.querySelector('.popup .close');
+        const popupForm = document.getElementById('popupForm');
+
+        if (openPopupButton) {
+            openPopupButton.addEventListener('click', () => {
+                overlay.style.display = 'flex';
+            });
+        }
+
+        if (closeButton) {
+            closeButton.addEventListener('click', () => {
+                overlay.style.display = 'none';
+            });
+        }
+
+        if (overlay) {
+            overlay.addEventListener('click', (event) => {
+                if (event.target === overlay) {
+                    overlay.style.display = 'none';
+                }
+            });
+        }
+
+        if (popupForm) {
+            popupForm.addEventListener('submit', (event) => {
+                event.preventDefault();
+                overlay.style.display = 'none';
             });
         }
     });
